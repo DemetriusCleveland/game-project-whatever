64652dfb68144d40a568b777f40e04dbce484947
Demetrius Cleveland <demetrius.cleveland14@gmail.com>
2017-11-21 15:50:32 -0600

    save0

---
+++ text adventure game.py
@@ -0,0 +1,5 @@
+'''Francis Magilicutty, private eye, receives a case of a missing girl said to be spotted near an 
+abandoned college, Sammy Systems University. Arriving on campus, the calm, Fall climate was nothing by 
+eerie. After paying the Uber driver,with no tip, he arrives on the campus "Just Talk" area and cleans up
+his mess.
+'''
