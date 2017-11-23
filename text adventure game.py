3369b5ee6ed7e0c895718454b5c51fae512ed812
NcL99 <nicholas.lloyd@ttu.edu>
2017-11-23 11:52:18 -0600

    airplane_save1

--- text adventure game.py
+++ text adventure game.py
@@ -7,8 +7,16 @@ his mess.
 #[Name, description/hint, HP, baseATK, attack1Name, attack1Uses, attack1Multiplier, attack2Name, attack2Uses, attack2Multiplier, baseDEF, dropID(int that determine's type of loot awarded on death)
 #attacker and defender objects are lists with their stats, preassigned from character status
 
+
+#separating into two combats for attack1 and attack2
 def combat1(attacker, defender):
-    defender[2]=(attacker[3]*attacker[6])-defender[10]
+#check if attacker has at least 1 charge for the attack
+    if(attacker[5] > 0):
+#lowers defender's hp based on attacker's base attack and particular attack multiplier minus the defender's def
+        defender[2]= defender[2]-((attacker[3]*attacker[6])-defender[10])
+        attacker[5] = attacker[5]-1
+    else:
+        print("Attacker does not have any charge left for that move!")
 
 def combat2(attacker, defender):
     
