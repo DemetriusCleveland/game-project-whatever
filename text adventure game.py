aab322fe18b7633aee29ac4c9a5b0ab9fd52a3c1
Demetrius Cleveland <demetrius.cleveland14@gmail.com>
2017-11-28 09:35:47 -0600

    new save dude

--- text adventure game.py
+++ text adventure game.py
@@ -1,7 +1,18 @@
+def map():
+    '''This function is called simply to display the map.'''
+    return 0
+
+def pickItem():
+    '''Once an item is obtained, this function can be called to select it from a menu.'''
+    return 0
+
+def restartAfterDeath():
+    '''After the character dies, this function is called to
+    ask the user if he wants to restart or quit the game.'''
+    return 0
+
 '''Francis Magilicutty, private eye, receives a case of a missing girl said to be spotted near an 
 abandoned college, Sammy Systems University. Arriving on campus, the calm, Fall climate was nothing by 
 eerie. After paying the Uber driver,with no tip, he arrives on the campus "Just Talk" area and cleans up
 his mess.
 '''
-#Nerd Shit
-#True that
