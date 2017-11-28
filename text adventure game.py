98d8b4e87cae608b063f539ec096fb1165ef2dcc
Demetrius Cleveland <demetrius.cleveland14@gmail.com>
2017-11-28 09:55:41 -0600

    even newer changes

--- text adventure game.py
+++ text adventure game.py
@@ -2,15 +2,24 @@ def map():
     '''This function is called simply to display the map.'''
     return 0
 
-def pickItem():
+def pickItem(item, itemList):
     '''Once an item is obtained, this function can be called to select it from a menu.'''
-    return 0
+    itemList.append(item)
+    print(item, "has been added to your inventory!")
+    #return 0
 
-def restartAfterDeath():
+def restartAfterDeath(answer):
     '''After the character dies, this function is called to
     ask the user if he wants to restart or quit the game.'''
-    return 0
+    if answer=="y":
+        'Start at the beginning'
+    else:
+        "Quit the game"
+    #return 0
 
+francis=[francis,...]
+items=[]#At the start, the item is empty
+enemy1=[enemy1,...]
 '''Francis Magilicutty, private eye, receives a case of a missing girl said to be spotted near an 
 abandoned college, Sammy Systems University. Arriving on campus, the calm, Fall climate was nothing by 
 eerie. After paying the Uber driver,with no tip, he arrives on the campus "Just Talk" area and cleans up
